ecf75b2aa045f12dd0db2b189f85264e73b8b5d7
Nikhil Pawar <63259987+nikhilp15@users.noreply.github.com>
2021-03-19 14:17:09 +0530

    Add files via upload

---
+++ file1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("this is initial file 1");
+    return 0;
+}
---
+++ file2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("this is initial file 2");
+    return 0;
+}
